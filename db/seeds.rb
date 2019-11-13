d01eb5873397c6b050445f283357d3fa1a4b532e
Jonathan Pein <jonathan.o.k.pein@gmail.com>
2019-11-13 15:36:22 +0000

    challenge 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'japanese',
+    phone_number:        "07500701738"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56b Shoreditch High St, London E1 6PQ',
+    category:  'chinese',
+    phone_number:        "07500701724"
+  },
+  {
+    name:         'Hello',
+    address:      '56c Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "07500701731"
+  },
+  {
+    name:         'Fine',
+    address:      '56d Shoreditch High St, London E1 6PQ',
+    category:  'japanese',
+    phone_number:        "07500701734"
+  },
+  {
+    name:         'Pizza South',
+    address:      '56e Shoreditch High St, London E1 6PQ',
+    category:  'french',
+    phone_number:        "02500701734"
+  },
+  {
+    name:         'Pizza North',
+    address:      '56f Shoreditch High St, London E1 6PQ',
+    category:  'belgian',
+    phone_number:        "04500701734"
+  },
+  {
+    name:         'Pizza West',
+    address:      '56g Shoreditch High St, London E1 6PQ',
+    category:  'chinese',
+    phone_number:        "06500701734"
+  },
+  {
+    name:         'Assembly',
+    address:      '56h Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "07500601734"
+  },
+  {
+    name:         'General',
+    address:      '54 Shoreditch High St, London E1 6PQ',
+    category:  'belgian',
+    phone_number:        "07505701734"
+  },
+  {
+    name:         'Wagon',
+    address:      '52 Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:        "03500701734"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
